f6e1086800e4c9e48f9a703245378bfc2c448def
vrik123 <42827942+vrik123@users.noreply.github.com>
2020-12-24 15:01:37 +0530

    Update db.php

--- php/online-shopping-system/db.php
+++ php/online-shopping-system/db.php
@@ -3,7 +3,7 @@
 $servername = "100.26.111.239";
 $username = "admin";
 $password = "admin123";
-$db = "database";
+$db = "database1";
 
 // Create connection
 $con = mysqli_connect($servername,$username,$password,$db);
